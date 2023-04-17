127f72be3724be4d7a1fd92080de9cc82cc91634
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-04-17 17:08:39 +0200

    User entity created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const {connectDB} =  require("./mongo/connection");
 const cors = require('cors');
+const userRouter = require('./routers/user.router.js')
 const app = express();
+connectDB().then(() => console.log("Connected to database!"))
+
+
+
 app.use(cors());
 app.use(express.json());
-
-
-connectDB().then(() => console.log("Connected to database!"))
+app.use(userRouter);
 
 const server = app.listen(3001, () => {
     console.log('Server is up and running ⚡')
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
 
+const url = 'http://127.0.0.1:27605/users'
+const testData = {
+	name: "hola",
+	lastname: "holita",
+	email:"riderelli.ben@gmail.com",
+	password:"hola"	
+}
+
+const test = () => {
+  fetch(url,{
+  method:'POST',
+  headers: {
+      "Content-Type": "application/json",
+    },
+  body: JSON.stringify(testData)
+ })
+ .then(res => {res.json()})
+ .then(data => {
+  console.log(testData)
+  console.log(data)})   
+  .catch(err => {
+    console.log(JSON.stringify(testData))
+    console.log(err)})
+}
+
+
+
 function App() {
     return (
     <div>
-        Your App here
+        <button
+          onClick={() => test()}
+        > test </button>
     </div>
 
 
