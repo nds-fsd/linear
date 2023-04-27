34a5868ca7b530ddc9d0c3e130cf5084e7571749
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-04-27 14:28:20 +0200

    changes

--- frontend/src/components/kanban/kanban.jsx
+++ frontend/src/components/kanban/kanban.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
 import DroppingArea from "./droppingarea/droppingarea";
 import DraggableWidget from "./draggablewidget/draggablewidget";
-import kanbanStyle from './kanban.module.css'
+import kanbanStyle from "./kanban.module.css";
 
 function Kanban() {
+  //   Hacer que pueda recibir un array con "estados" y generar una columna y un useState para cada uno
+  //   con el objetivo de hacerlo más reutilizable
 
+  const [groupOneWidgets, setGroupOneWidgets] = useState(["Jhon", "Talabot"]);
+  const [groupTwoWidgets, setGroupTwoWidgets] = useState(["Indio", "Solari"]);
+  const [groupThreeWidgets, setGroupThreeWidgets] = useState([
+    "Peter",
+    "Parker",
+  ]);
+  const [groupFourWidgets, setGroupFourWidgets] = useState(["Mick", "Jagger"]);
 
-  const [groupOneWidgets, setGroupOneWidgets] = useState(["1","4"]);
-  const [groupTwoWidgets, setGroupTwoWidgets] = useState(["2","3"]);
-  
   return (
     <div className={kanbanStyle.kanbanArea}>
       <DroppingArea setWidgets={setGroupOneWidgets} widgets={groupOneWidgets}>
         {groupOneWidgets?.map((widget, index) => (
-          <DraggableWidget 
-          widgetList={groupOneWidgets}
-          updateWidgetList={setGroupOneWidgets}
-          title={widget} 
-          key={index}
-          index={index}
+          <DraggableWidget
+            widgetList={groupOneWidgets}
+            updateWidgetList={setGroupOneWidgets}
+            title={widget}
+            key={index}
+            index={index}
           />
         ))}
       </DroppingArea>
@@ -26,11 +32,36 @@ function Kanban() {
       <DroppingArea setWidgets={setGroupTwoWidgets} widgets={groupTwoWidgets}>
         {groupTwoWidgets?.map((widget, index) => (
           <DraggableWidget
-          widgetList={groupTwoWidgets} 
-          updateWidgetList={setGroupTwoWidgets}
-          title={widget} 
-          key={index} 
-          index={index}
+            widgetList={groupTwoWidgets}
+            updateWidgetList={setGroupTwoWidgets}
+            title={widget}
+            key={index}
+            index={index}
+          />
+        ))}
+      </DroppingArea>
+      <DroppingArea
+        setWidgets={setGroupThreeWidgets}
+        widgets={groupThreeWidgets}
+      >
+        {groupThreeWidgets?.map((widget, index) => (
+          <DraggableWidget
+            widgetList={groupThreeWidgets}
+            updateWidgetList={setGroupThreeWidgets}
+            title={widget}
+            key={index}
+            index={index}
+          />
+        ))}
+      </DroppingArea>
+      <DroppingArea setWidgets={setGroupFourWidgets} widgets={groupFourWidgets}>
+        {groupFourWidgets?.map((widget, index) => (
+          <DraggableWidget
+            widgetList={groupFourWidgets}
+            updateWidgetList={setGroupFourWidgets}
+            title={widget}
+            key={index}
+            index={index}
           />
         ))}
       </DroppingArea>
