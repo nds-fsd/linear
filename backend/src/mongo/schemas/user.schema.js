b0a1f2c8819edb7a23a9d22e42bcdba9f88aa560
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-05-02 18:18:59 +0200

    password hashing and backend user registration

--- backend/src/mongo/schemas/user.schema.js
+++ backend/src/mongo/schemas/user.schema.js
@@ -1,10 +1,13 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
- //const regex =  [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email']
+//const regex =  [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email']
 
-const userSchema = new Schema(
+const UserSchema = new Schema(
   {
-    name: { type: String, required: true },
+    firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -12,6 +15,48 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-const User = model("User", userSchema);
+// //esta funcion se ejecuta "antes" de guardar cualquier usuario en Mongo
+UserSchema.pre("save", function (next) {
+  const user = this;
+  //si no se ha cambiado la contraseña, seguimos
+  if (!user.isModified("password")) return next();
+
+  //brcypt es una libreria que genera "hashes", encriptamos la contraseña
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // si no ha habido error en el encryptado, guardamos
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+// * Method to generate the JWT (You choose the name)
+UserSchema.methods.generateJWT = function () {
+  const today = new Date();
+  const expirationDate = new Date();
+
+  expirationDate.setDate(today.getDate() + 60);
+
+  let payload = {
+    id: this._id,
+    name: this.firstName,
+    email: this.email,
+  };
+  // * This method is from the json-web-token library (who is in charge to generate the JWT
+  return jwt.sign(payload, secret, {
+    expiresIn: parseInt(expirationDate.getTime() / 1000, 10),
+  });
+};
+
+const User = model("User", UserSchema);
 
 module.exports = User;
