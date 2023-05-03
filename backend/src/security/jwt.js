518b89dcf62263b018f12af1a18947a394615f7e
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-05-03 11:29:35 +0200

    aded email regex

--- backend/src/security/jwt.js
+++ backend/src/security/jwt.js
@@ -7,7 +7,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 
 
-const regex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailRegex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const authRouter = express.Router();
 
@@ -25,7 +25,7 @@ authRouter.post("/register", async (req, res) => {
       .status(400)
       .json({ error: { email: "Email already registered" } });
   } else {
-    if (!data.email.match(regex)){
+    if (!data.email.match(emailRegex)){
       return res
       .status(400)
       .json({ error: { email: "Insert a valid email" } });
