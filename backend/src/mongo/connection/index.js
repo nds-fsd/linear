0ac41c4af904dcd912767938c177e1e25b1a43dd
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-05-05 17:46:52 +0200

    small changes

--- backend/src/mongo/connection/index.js
+++ backend/src/mongo/connection/index.js
@@ -12,7 +12,6 @@ exports.connectDB = async () => {
   mongoose.set("strictQuery", false);
   try {
     await mongoose.connect(dbUrl);
-    // mongoose.connect(dbUrl);
     const mongo = mongoose.connection;
     mongo.on("error", (error) => console.error(error));
   } catch (e) {
