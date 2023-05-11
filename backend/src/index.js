190c1a2e36187586114896324bb0481fbc306be3
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-05-11 10:55:17 +0200

    meaningfull message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,3 @@
-// const userRouter = require('./routers/user.router.js')
-// const projectRouter= require('./routers/project.router.js')
-// const taskRouter= require('./routers/task.router.js')
 const express = require("express");
 const { connectDB } = require("./mongo/connection");
 const cors = require("cors");
@@ -10,10 +7,6 @@ connectDB().then(() => console.log("Connected to database!"));
 
 const dotenv = require("dotenv");
 dotenv.config();
-
-// app.use(userRouter);
-// app.use(projectRouter);
-// app.use(taskRouter);
 app.use(cors());
 app.use(express.json());
 app.use(generalRouter);
