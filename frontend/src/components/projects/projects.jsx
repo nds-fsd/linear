aa8cf5f43c64ffd3774836895247b287dcab0b46
Benjamin Riderelli <riderelli.ben@gmail.com>
2023-06-21 12:55:45 +0200

    added filtering

--- frontend/src/components/projects/projects.jsx
+++ frontend/src/components/projects/projects.jsx
@@ -7,12 +7,18 @@ import AddProjectModal from "./addprojectmodal/addprojectmodal";
 import { handleSearch } from "../../utils/searchInput";
 
 const Projects = () => {
+  const { teams, userSessionContext } = useContext(Context);
+  const { id } = userSessionContext;
+
+  const initialData = teams.filter((team) => {
+    const userIdsArray = team.users?.map((user) => user._id);
+    return userIdsArray?.includes(id);
+  });
+
   const [activeView, setActiveview] = useState("list");
   const [showAddProjectModal, setShowAddProjectModal] = useState(false);
-  const { teams, userSessionContext } = useContext(Context);
   const [searchbarFilter, setSearchbarFilter] = useState("");
-  const [ filteredTeams, setFilteredTeams] = useState([])
-  const {id} = userSessionContext
+  const [filteredTeams, setFilteredTeams] = useState(initialData);
 
   const handleEditModal = () => {
     console.log("hola");
@@ -25,29 +31,29 @@ const Projects = () => {
   const handleAddProjectModal = () => {
     setShowAddProjectModal(true);
   };
-  
-const filteredTeams = teams.filter(team => {
-  const userIdsArray = team.users?.map(user=> user._id)
-  return userIdsArray?.includes(id)
-})
 
+  const handleSearch = (value, data) => {
+    if (!data || !value) {
+      return;
+    }
+    const projects = initialData.filter((team) => team.title.includes(value));
+    return projects;
+  };
 
+  useEffect(() => {
+    setFilteredTeams(initialData);
 
-useEffect(() => {
-  if (searchbarFilter) {
-
-  } else if(!searchbarFilter){
-
-  }
-}, [searchbarFilter]);
-
-
-
-
-
-
-
-
+    if (searchbarFilter) {
+      setFilteredTeams((prevState) => {
+        const filteredProjects = handleSearch(searchbarFilter, initialData);
+        if (filteredProjects.length === 0) {
+          return [];
+        } else {
+          return filteredProjects;
+        }
+      });
+    }
+  }, [teams, searchbarFilter]);
 
   const filterData = { type: "simple" };
   return (
@@ -59,6 +65,7 @@ useEffect(() => {
         btntitle="Project"
         filterData={filterData}
         btnFunction={handleAddProjectModal}
+        setSearchbarFilter={setSearchbarFilter}
       />
       <ProjectListView
         data={filteredTeams}
